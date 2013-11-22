436a8a61ee3b1832ac37ffd8d85c7d97824ffcc9
Edson Lima <edsonlima@edson-notebook.(none)>
2013-11-22 10:20:16 -0300

    adicionar pagina home

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Portaledsis::Application.routes.draw do
 
-  get "paginas/produtos"
+  resources "paginas"
 
-  get "paginas/clientes"
+  match "pagina/home"     => "application#index"
+  match "pagina/produtos" => "paginas#produtos"
+  match "pagina/clientes" => "paginas#clientes"
+  match "pagina/sobre"    => "paginas#sobre"
+  match "pagina/contato"  => "paginas#contato"
 
-  get "paginas/sobre"
 
-  get "paginas/contato"
+  #Só para saber que é possivel chamar a pagina diretamente
+  #get "paginas/clientes"
+
 
   root :to => 'application#index'
 
-  match "/paginas/sobre" => "paginas#sobre"
 
 end
